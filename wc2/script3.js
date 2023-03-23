37664778fc617eb16c9b6fbf88097c03bcaa14ae
chris <chris.ilunga-tshamakeji@student.ehb.be>
2023-03-23 21:18:54 +0100

    wc2 oef 5-6 verbeterd

--- wc2/script3.js
+++ wc2/script3.js
@@ -1,77 +1,58 @@
 'use strict';
 //oef 5-6
 
-/*START OEF 5*/ 
+//let coursesArray = [];
+let data = [];
+
 let student = {
-    naam: document.getElementById('naam').value,
-    age: document.getElementById('leeftijd').value,
-    degree: document.getElementById('degree').value,
-    courses: [document.getElementById('courses').value],
-   
-}
-let name = prompt("naam");
-let age = prompt("age");
-let degree = prompt("degree");
-let courses = prompt("coures");
+    naam: 'default',
+    age:'88888' ,
+    degree:'zero' ,
+    courses: [],
+    setPersonalDetails(details) {
+        
+        [this.naam, this.age, this.degree] = details;
+        //console.log(details);
+    },
 
-
-
-
-//pas sur que c'est ca le verkorte versie 
-//comme ca sa marche aussi quand je rajoute les haakjes il capte direct 
-//que je veux le functie mais quand je mets ap ils me montre ce qu'il y a
-//dans le variable exemple => tema console.log sans les haakjes
- setPersonalDetails: function (details ) {
+    addCourses (...courses) /* ici on utilise le spread operartor pcq le array argument doit catch des waarden or
+    qu'avec setPersonalDetails "details" a deja des arguments a l'interieur dcp ce qui possede déja qu'on utilise mais avec 
+    le argumet "courses" on prends des arguments pour les mettre dans le array */ {
+        /*cette methode avec un autre array marche mais il faut faire avec 1 seule array 
+         coursesArray.push(`${courses},`)*/
+         this.courses = [...this.courses, ...courses];
+         /*ici on dit que courses est egal a lui meme pour qu'il perds pas les cours qu'il la déjà en magasiner
+         puis on dit qu'il est aussi égal a "...courses" pcq comme on appelle plusiseurs fois le functie on veut que 
+         chaque nouveau waarde ce rajoute dans student.courses*/
+        console.log(courses);
     
-    [student.naam, student.age,student.degree] = ['Mike Derycke','29','Toegepaste Informatica'];
-    return student.naam + student.age +student.degree;
-}
+    },
 
-let details = ['Mike Derycke','29','Toegepaste informatica' ] ;
-console.log(personalDetails(details));
-student.personalDetails(details);
-console.log(personalDetails);
-
-// THE END OEF 5
-
-// OEF 6 RIEN REUSII SA MERE
-
-let coursesArray = [student.courses];
-let coursesAdder = document.getElementById('addCourses');
-coursesAdder.addEventListener('click', function () {
-    addCourses();
-})
-let limit = 0;
-let addCourses = function addCourses() {
-    //je cpate pas pourquoi sans ce stap la les propreties de student n'ont pas de value
-    let courses = document.getElementById('courses').value;
-    student.courses = courses;
-    const [...addCourses] = rest ;
-    console.log(rest);
-
-    if (student.courses !== 0) {
-        coursesArray[limit] = student.courses;
-        limit++;
-        console.log(coursesArray[limit]);
+    showStudent() {
+        let show = ` Mijn naam is ${this.naam}. Ik ben ${this.age} jaar oud en volg ${this.degree} aan EhB
+        Mijn vakken zijn: ${this.courses.join(', ')}`;
+        /*`
+                    Mijn naam is ${this.naam}. Ik ben ${this.age} jaar oud en volg ${this.degree} aan EhB
+                    Mijn vakken zijn: ${coursesArray}
+                    `;*/
+        console.log(show);
+        //console.log(data);
         
-        
-    }
+    },
 
+    
 }
 
 
-let showStudent = function showStudent() {
-    let show = `
-                Mijn naam is ${student.naam}. Ik ben ${student.age} jaar oud en volg ${student.degree} aan EhB
-                Mijn vakken zijn: ${coursesArray}
-                `;
-    console.log(show);
-}
-let prompt = function prompt() {
-    setPersonalDetails();
-    showStudent();
-}
-let boutton = document.getElementById('button');
-boutton.addEventListener('click', function () {
-    prompt();
-})
+data.push(prompt("naam"));
+data.push(prompt("age"));
+data.push(prompt("degree"));
+
+
+student.addCourses('qqs','hjfjf');
+student.addCourses('undo');
+student.setPersonalDetails(data);
+student.showStudent();
+
+
+
