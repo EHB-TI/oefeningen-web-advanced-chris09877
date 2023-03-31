64de75f75eac6cdf3ee1453d7a80e6fca2bd0469
chris <chris.ilunga-tshamakeji@student.ehb.be>
2023-03-31 10:47:12 +0200

    wc4A gemaakt maar nog niet verbeterd

---
+++ wc4A/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+let randomNumber =5;
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", event =>{
+
+    let number = document.querySelector("input").value;
+    console.log(number);
+    let promise = compareNumber(number);
+    promise.finally( ()=> {}).then(
+        result => alert(result),
+        error => alert(error)
+    )
+});
+
+
+function compareNumber(number){
+    return  new Promise(function(resolve, reject){
+        
+        if (number >= 1 && number <= 20) {
+            if (number < randomNumber) {
+                reject(new Error('Het mysterieuze getal is lager. Raad nog eens!'));
+            }
+    
+            if (number > randomNumber ) {
+                reject(new Error('Het mysterieuze getal is hoger. Raad nog eens!'));
+            }
+            if (number == randomNumber) {
+                //bizzarement quand je mets 3 fois le symbole est égal ca ne marche pas"
+                resolve('Je hebt het mysterieuze nummer geraden!');
+            }
+        }
+        
+        
+        else
+        {
+            reject(new Error('Dat is geen geldig nummer'));
+        }
+    });
+    
+};
